adead3183cca35d534da96fc6ee5083903c3a160
SochaStanislaw <socha.stanislaw95@gmail.com>
2023-03-31 23:01:46 +0200

    backup#2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector("ul.gallery");
+
+for (const pic of galleryItems) {
+  const item = `<div class="gallery__item">
+  <a class="gallery__link" href="${pic.original}">
+  <img
+    class="gallery__image"
+    src="${pic.preview}"
+    data-source="${pic.original}"
+    alt="${pic.description}"
+  />
+</a>
+</div>`;
+  gallery.insertAdjacentHTML("beforeend", item);
+}
+
+gallery.addEventListener("click", (ev) => {
+  ev.preventDefault();
+  //   if (ev.target.tagName !== "IMG") return;
+  const popUpPic = basicLightbox.create(
+    `<img src="${ev.target.dataset.source}">`
+  );
+  popUpPic.show();
+});
