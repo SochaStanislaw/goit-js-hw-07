c2662336698b1e2ad87549c045cd3682c13c3a44
SochaStanislaw <socha.stanislaw95@gmail.com>
2023-04-01 23:12:07 +0200

    backup#3

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -18,9 +18,32 @@ for (const pic of galleryItems) {
 
 gallery.addEventListener("click", (ev) => {
   ev.preventDefault();
-  //   if (ev.target.tagName !== "IMG") return;
+  if (ev.target.tagName !== "IMG") return;
   const popUpPic = basicLightbox.create(
     `<img src="${ev.target.dataset.source}">`
   );
   popUpPic.show();
 });
+
+gallery.addEventListener("keydown", (ev) => {
+  if (ev.key === "Escape") {
+    // alert("test");
+    popUpPic.close();
+  }
+});
+
+// gallery.addEventListener("click", (ev) => {
+//   ev.preventDefault();
+//   const popUpPic = basicLightbox.create(
+//     `<img src="${ev.target.dataset.source}"/>`
+//   );
+//   {
+//     onShow: (popUpPic) => {
+//       window.addEventListener("keydown", (ev) => {});
+//       if (ev.key === "Escape") {
+//         popUpPic.close();
+//       }
+//     };
+//   }
+//   popUpPic.show();
+// });
